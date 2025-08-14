d660dae0a6e715d053d7feb9c472314dddaf2da8
Caio-Brito-Lima <178065@cloud.rangitoto.school.nz>
2025-08-15 10:31:25 +1200

    Update hello.py

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+import tkinter
+m = tkinter.Tk()
+m.title("Hello World")
+label = tkinter.Label(m, text="Hello, World!")
+label.pack()
+m.geometry("200x100")
+m.button = tkinter.Button(m, text="Close", command=m.destroy)
+m.button.pack()
+m.mainloop()
